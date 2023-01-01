da2a537bbb8a3f86af86a4b99e49267f1f2ddf9e
talop01 <12220043.gcit@rub.edu.bt>
2023-01-01 19:01:29 +0600

    Modified the file for heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ form.addEventListener("submit",(e) => {
     locationPlace.textContent = "loading..."
     forecastPlace.textContent = ""
     
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 locationPlace.textContent = data.error
